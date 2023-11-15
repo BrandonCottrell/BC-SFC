ff2b43867fbddf34c8b57a5da7904538648a3319
Brandon Cottrell <bcottrell@my.wctc.edu>
2023-11-15 12:17:05 -0600

    BC SFC

---
+++ vue-components-sfc/src/components/ViewModal.vue
@@ -0,0 +1,86 @@
+<script>
+import { Trip } from '../models/Trip';
+
+export default {
+    name: "ViewTrip",
+    data(){
+        return {
+            editTrip: {
+                ...this.modelValue
+            },
+            uid: 'eim-' + Math.floor(Math.random() * 10e16),
+        }
+    },
+    props: {
+        modelValue: {
+            type: Object,
+            required: true,
+        }
+    },
+    emits: ['update:modelValue'],
+    methods: {
+        editIt: function () {
+            Object.assign(this.modelValue, this.editTrip);
+        },
+    }
+}
+</script>
+
+<template>
+<div class="modal fade" id="viewTripModal" tabindex="-1" role="dialog" aria-labelledby="viewTripModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+            <slot name="header">
+                <h5 class="modal-title" id="viewTripModalLabel">Completed Trip</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+          </slot>
+        </div>
+        <div class="modal-body">
+            <slot>
+                <form>
+                    <div class="form-group">
+                    <label for="nameOfTrip">Name of Trip</label>
+                    <input v-model="modelValue.name" type="text" class="form-control" id="NameOfTrip" placeholder="Trip Name..." disabled>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <label for="start">Start date: </label>
+                            <input v-model="modelValue.startDate" type="date" class="form-control" id="start" name="trip-start" min="2023-11-01" max="2100-12-31"  disabled>
+                        </div>
+                        <div class="col">
+                            <label for="start">End date: </label>
+                            <input v-model="modelValue.endDate" type="date" class="form-control" id="end" name="trip-end" min="2023-11-01" max="2100-12-31"  disabled>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="tripPrice">Trip Price</label>
+                        <select v-model="modelValue.price" id="tripPrice" class="form-control" disabled>
+                        <option selected> </option>
+                        <option>$</option>
+                        <option>$$</option>
+                        <option>$$$</option>
+                        </select>
+                    </div>
+                </form>
+            </slot>
+        </div>
+        <div class="modal-footer">
+            <slot name="footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </slot>
+        </div>
+      </div>
+    </div>
+</div>
+        
+    
+</template>
+
+<style>
+
+</style>
---
+++ vue-components-sfc/src/models/Trip.js
@@ -0,0 +1,11 @@
+function Trip(name, startDate, endDate, price, favorited, completed){
+    this.name = name ?? '';
+    this.startDate = startDate ?? '2024-01-01';
+    this.endDate = endDate ?? '2024-12-31';
+    this.price = price ?? '';
+    this.favorited = favorited ?? true;
+    this.completed = completed ?? false;
+}
+Trip.type = 'Trip';
+
+export {Trip};
